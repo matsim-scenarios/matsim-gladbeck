44b0bfa1249d801d88739bb2eae73c36a8a04949
GregorRyb <rybczak@vsp.tu-berlin.de>
2022-03-18 15:27:18 +0100

    continued scenario cut out

--- src/main/java/org/matsim/analysis/LinksInShp.java
+++ src/main/java/org/matsim/analysis/LinksInShp.java
@@ -19,12 +19,8 @@
  * *********************************************************************** */
 package org.matsim.analysis;
 
-import com.opencsv.CSVParser;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
-import com.opencsv.ICSVParser;
-import com.opencsv.validators.LineValidatorAggregator;
-import com.opencsv.validators.RowValidatorAggregator;
+
+import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.prep.PreparedGeometry;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
@@ -33,73 +29,43 @@ import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 
-import java.io.*;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
-import java.util.stream.Collectors;
-import static com.opencsv.CSVReader.*;
-import static com.opencsv.ICSVWriter.*;
+
 
 public class LinksInShp {
 
+    private static final Logger log = Logger.getLogger(LinksInShp.class);
+
     public static void main(String[] args) {
-
-        String hamburgHVVShape = "../../svn/shared-svn/projects/realLabHH/data/hamburg_shapeFile/hamburg_hvv/hamburg_hvv.shp";
-//		String hamburgCityShape = "../../svn/shared-svn/projects/realLabHH/data/hamburg_shapeFile/hamburg_city/hamburg_stadtteil.shp";
-
-
-        List<PreparedGeometry> hamburgHVVGeoms = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(hamburgHVVShape));
-        Network hamburgNetwork = NetworkUtils.readTimeInvariantNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/reallab2030plus/input/network/hamburg-v2.0-reallab2030plus-network-with-pt.xml.gz");
-
-        writeLinksInShpCSV(hamburgNetwork, hamburgHVVGeoms, "../../svn/public-svn/matsim/scenarios/countries/de/hamburg/hamburg-v2/hamburg-v2.0/reallab2030plus/input/network/hamburg-v2.0-reallab2030plus-network-links-in-hvvArea.tsv");
+        String gladbeckShape = "../../shared-svn/projects/GlaMoBi/data/shp-files/Gladbeck.shp";
+        List<PreparedGeometry> gladbeckGeoms = ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(gladbeckShape));
+        Network ruhrgebietNetwork = NetworkUtils.readTimeInvariantNetwork("https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.network_resolutionHigh-with-pt.xml.gz");
+        writeLinksInToNetwork(ruhrgebietNetwork, gladbeckGeoms, "../../shared-svn/projects/GlaMoBi/networkLinksWithinGladbeck.xml.gz");
     }
 
-    static void writeLinksInShpCSV(Network network, List<PreparedGeometry> preparedGeometryList, String outputFilePath){
-        if(! outputFilePath.endsWith(".tsv")) throw new IllegalArgumentException("output file path should end with .tsv");
-
-        Set<String[]> linkIds = network.getLinks().values().stream()
-                .filter(link -> ShpGeometryUtils.isCoordInPreparedGeometries(link.getCoord(), preparedGeometryList))
-                .map(link -> new String[]{link.getId().toString()})
-                .collect(Collectors.toSet());
-
-        try {
-            CSVWriter writer = new CSVWriter(new FileWriter(outputFilePath), '\t', NO_QUOTE_CHARACTER, DEFAULT_ESCAPE_CHARACTER, DEFAULT_LINE_END);
-            writer.writeNext(new String[]{"linkId"});
-            writer.writeAll(linkIds);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /**
-     * wpuld do this with CSVReader but we did not use default settings when writing this...
-     * @param filePath
-     * @return
-     * @throws FileNotFoundException
-     */
-    static Set<String> readLinkIdStrings(String filePath) throws FileNotFoundException {
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-
-        Set<String> linkIds = new HashSet<>();
-
-        try {
-            reader.readLine();
-            String line = reader.readLine();
-            while(line != null){
-                linkIds.add(line);
-                line = reader.readLine();
+    static void writeLinksInToNetwork(Network network, List<PreparedGeometry> preparedGeometryList, String outputFilePath){
+        Set<Id<Link>> linkIds = new HashSet<>();
+        log.info("Parsing links");
+        for (Link link : network.getLinks().values()) {
+            if (ShpGeometryUtils.isCoordInPreparedGeometries(link.getCoord(), preparedGeometryList)) {
+                linkIds.add(link.getId());
             }
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return linkIds;
+
+        for (Link link: network.getLinks().values()) {
+            if (linkIds.contains(link.getId())) {
+                //keep link
+                log.info("Keep Link "+link.getId()+"because in shp File");
+            }
+            else {
+                network.removeLink(link.getId());
+            }
+        }
+        NetworkUtils.writeNetwork(network, outputFilePath);
     }
+
+
 
 }
