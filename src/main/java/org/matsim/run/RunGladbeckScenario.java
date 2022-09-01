d07dec47c1c25e636e0ee57bddf9918286144f38
Christian Rakow <rakow@vsp.tu-berlin.de>
2022-09-01 11:14:07 +0200

    remove hardcoded single iteration

--- src/main/java/org/matsim/run/RunGladbeckScenario.java
+++ src/main/java/org/matsim/run/RunGladbeckScenario.java
@@ -23,98 +23,90 @@ import org.matsim.run.policies.Tempo30Zone;
 import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 import picocli.CommandLine;
 import playground.vsp.scoring.IncomeDependentUtilityOfMoneyPersonScoringParameters;
+
 import java.util.HashMap;
 import java.util.List;
 
 @CommandLine.Command(header = ":: Gladbeck Scenario ::", version = "v1.0")
 public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 
-    private static final Logger log = Logger.getLogger(RunGladbeckScenario.class);
+	private static final Logger log = Logger.getLogger(RunGladbeckScenario.class);
 
-    @CommandLine.Option(names = "--ptFlat", defaultValue = "false", description = "measures to allow car users to have free pt")
-    private boolean ptFlat;
+	@CommandLine.Option(names = "--ptFlat", defaultValue = "false", description = "measures to allow car users to have free pt")
+	private boolean ptFlat;
 
-    @CommandLine.Option(names = "--schoolClosure", defaultValue = "false", description = "measures to ban car on certain links")
-    boolean schoolClosure;
+	@CommandLine.Option(names = "--schoolClosure", defaultValue = "false", description = "measures to ban car on certain links")
+	boolean schoolClosure;
 
-    @CommandLine.Option(names = "--tempo30Zone", defaultValue = "false", description = "measures to reduce car speed to 30 km/h")
-    boolean tempo30Zone;
+	@CommandLine.Option(names = "--tempo30Zone", defaultValue = "false", description = "measures to reduce car speed to 30 km/h")
+	boolean tempo30Zone;
 
-    static HashMap<Id<Person>, Integer> personsEligibleForPtFlat = new HashMap<>();
+	static HashMap<Id<Person>, Integer> personsEligibleForPtFlat = new HashMap<>();
 
-    public RunGladbeckScenario() {
-        //super("./scenarios/gladbeck-v1.0/input/gladbeck-v1.0-25pct.config.xml");
-        super("./scenarios/gladbeck-v1.0/input/gladbeck-v1.0-25pct.config.xml");
-    }
+	public RunGladbeckScenario() {
+		//super("./scenarios/gladbeck-v1.0/input/gladbeck-v1.0-25pct.config.xml");
+		super("./scenarios/gladbeck-v1.0/input/gladbeck-v1.0-25pct.config.xml");
+	}
 
-    public static void main(String[] args) {MATSimApplication.run(RunGladbeckScenario.class, args);}
+	public static void main(String[] args) {
+		MATSimApplication.run(RunGladbeckScenario.class, args);
+	}
 
-        @Override
-        public Config prepareConfig(Config config) {
-            var preparedConfig = super.prepareConfig(config);
-            log.info("changing config");
-            preparedConfig.controler().setLastIteration(1);
-            // TODO: create separate config
-            //preparedConfig.network().setInputFile("/Users/gregorr/Desktop/Test/GlaMoBi/reduced_metropole-ruhr-v1.0.network_resolutionHigh-with-pt-with-bike.xml.gz");
-            //preparedConfig.plans().setInputFile("/Users/gregorr/Desktop/Test/GlaMoBi/reduced_metropole-ruhr-v1.1-25pct.plans.xml.gz");
-            return preparedConfig;
-        }
+	@Override
+	protected void prepareScenario(Scenario scenario) {
+		super.prepareScenario(scenario);
 
-        @Override
-        protected void prepareScenario(Scenario scenario) {
-            super.prepareScenario(scenario);
-            
 
-            if (ptFlat) {
-                for (Person p: scenario.getPopulation().getPersons().values()) {
-                    Plan plan = p.getSelectedPlan();
-                    List<Leg> legs = TripStructureUtils.getLegs(plan);
-                    //only car users are allowed to enjoy the pt flatrate
-                    for (Leg leg: legs) {
-                        if (leg.getMode().equals(TransportMode.car)) {
-                            personsEligibleForPtFlat.put(p.getId(),0);
-                        }
-                    }
-                }
-                log.info("Number of Agents eligible for pt flat: " + personsEligibleForPtFlat.size());
-            }
+		if (ptFlat) {
+			for (Person p : scenario.getPopulation().getPersons().values()) {
+				Plan plan = p.getSelectedPlan();
+				List<Leg> legs = TripStructureUtils.getLegs(plan);
+				//only car users are allowed to enjoy the pt flatrate
+				for (Leg leg : legs) {
+					if (leg.getMode().equals(TransportMode.car)) {
+						personsEligibleForPtFlat.put(p.getId(), 0);
+					}
+				}
+			}
+			log.info("Number of Agents eligible for pt flat: " + personsEligibleForPtFlat.size());
+		}
 
-            if (tempo30Zone) {
-                Tempo30Zone.implementPushMeasuresByModifyingNetworkInArea(scenario.getNetwork(), ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource("/Users/gregorr/Desktop/Test_ScenarioCutOut/test.shp")));
-            }
+		if (tempo30Zone) {
+			Tempo30Zone.implementPushMeasuresByModifyingNetworkInArea(scenario.getNetwork(), ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource("/Users/gregorr/Desktop/Test_ScenarioCutOut/test.shp")));
+		}
 
-            if (schoolClosure) {
-                new SchoolRoadsClosure().closeSchoolRoads(scenario.getNetwork());
-            }
-        }
+		if (schoolClosure) {
+			new SchoolRoadsClosure().closeSchoolRoads(scenario.getNetwork());
+		}
+	}
 
-        @Override
-        protected void prepareControler(Controler controler) {
-            super.prepareControler(controler);
-            //use income-dependent marginal utility of money for scoring
-            log.info("Using income dependent scoring");
-            controler.addOverridingModule(new AbstractModule() {
-                @Override
-                public void install() {
-                    bind(ScoringParametersForPerson.class).to(IncomeDependentUtilityOfMoneyPersonScoringParameters.class).in(Singleton.class);
-                }
-            });
+	@Override
+	protected void prepareControler(Controler controler) {
+		super.prepareControler(controler);
+		//use income-dependent marginal utility of money for scoring
+		log.info("Using income dependent scoring");
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				bind(ScoringParametersForPerson.class).to(IncomeDependentUtilityOfMoneyPersonScoringParameters.class).in(Singleton.class);
+			}
+		});
 
-            if (ptFlat) {
-                log.info("using pt flat");
-                double ptConstant = controler.getConfig().planCalcScore().getScoringParameters("person").getModes().get(TransportMode.pt).getConstant();
-                log.info("pt constant is: " +ptConstant);
-                double ptDailyMonetaryConstant = controler.getConfig().planCalcScore().getScoringParameters("person").getModes().get(TransportMode.pt).getDailyMonetaryConstant();
-                log.info("daily monetary constant is: " + ptDailyMonetaryConstant);
-                PtFlatrate ptFlatrate = new PtFlatrate(personsEligibleForPtFlat, -1*ptConstant, -1*ptDailyMonetaryConstant);
-                controler.addOverridingModule(new AbstractModule() {
-                    @Override
-                    public void install() {
-                        addEventHandlerBinding().toInstance(ptFlatrate);
-                        addControlerListenerBinding().toInstance(ptFlatrate);
-                        install(new PersonMoneyEventsAnalysisModule());
-                    }
-                });
-            }
-        }
+		if (ptFlat) {
+			log.info("using pt flat");
+			double ptConstant = controler.getConfig().planCalcScore().getScoringParameters("person").getModes().get(TransportMode.pt).getConstant();
+			log.info("pt constant is: " + ptConstant);
+			double ptDailyMonetaryConstant = controler.getConfig().planCalcScore().getScoringParameters("person").getModes().get(TransportMode.pt).getDailyMonetaryConstant();
+			log.info("daily monetary constant is: " + ptDailyMonetaryConstant);
+			PtFlatrate ptFlatrate = new PtFlatrate(personsEligibleForPtFlat, -1 * ptConstant, -1 * ptDailyMonetaryConstant);
+			controler.addOverridingModule(new AbstractModule() {
+				@Override
+				public void install() {
+					addEventHandlerBinding().toInstance(ptFlatrate);
+					addControlerListenerBinding().toInstance(ptFlatrate);
+					install(new PersonMoneyEventsAnalysisModule());
+				}
+			});
+		}
+	}
 }
