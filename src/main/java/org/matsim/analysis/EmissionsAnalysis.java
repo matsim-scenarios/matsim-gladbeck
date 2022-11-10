f65d816db818a6bffcfea60b5e6aab42ef670a38
GregorRyb <rybczak@vsp.tu-berlin.de>
2022-11-10 15:22:33 +0100

    update analysis

--- src/main/java/org/matsim/analysis/EmissionsAnalysis.java
+++ src/main/java/org/matsim/analysis/EmissionsAnalysis.java
@@ -2,14 +2,9 @@ package org.matsim.analysis;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.application.analysis.emissions.AirPollutionByVehicleCategory;
 
-public class EmissionsAnalysis implements MATSimAppCommand {
+public class EmissionsAnalysis {
 
-    public static void main (String args []) throws Exception {
+    public static void main (String[] args) throws Exception {
         new AirPollutionByVehicleCategory().execute(args);
-    }
-
-    @Override
-    public Integer call() throws Exception {
-        return null;
     }
 }
--- src/main/java/org/matsim/analysis/NoiseAnalysisGladbeck.java
+++ src/main/java/org/matsim/analysis/NoiseAnalysisGladbeck.java
@@ -3,14 +3,10 @@ package org.matsim.analysis;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.application.analysis.noise.NoiseAnalysis;
 
-public class NoiseAnalysisGladbeck implements MATSimAppCommand {
+public class NoiseAnalysisGladbeck {
 
     public static void main (String args []) {
         new NoiseAnalysis().execute(args);
     }
 
-    @Override
-    public Integer call() throws Exception {
-        return null;
-    }
 }
