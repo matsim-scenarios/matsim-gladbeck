93c5f5f39d156aedd6e675fbc0423998cb5ee29e
GregorRyb <rybczak@vsp.tu-berlin.de>
2023-05-24 14:28:03 +0200

    continued with IntegrationTest

--- src/test/java/org/matsim/run/GladbeckIntegrationTest.java
+++ src/test/java/org/matsim/run/GladbeckIntegrationTest.java
@@ -2,10 +2,20 @@ package org.matsim.run;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.application.MATSimApplication;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.testcases.MatsimTestUtils;
 
 public class GladbeckIntegrationTest {
@@ -24,4 +34,30 @@ public class GladbeckIntegrationTest {
         MATSimApplication.execute(RunGladbeckScenario.class,
                 "run", "--10pct");
     }
+
+
+    private static Population createTestPopulation(Config config) {
+
+        Network network = NetworkUtils.readNetwork(config.network().getInputFile());
+
+        Population population = PopulationUtils.createPopulation(config);
+
+
+        Person carPerson = PopulationUtils.getFactory().createPerson(Id.createPersonId("carPerson"));
+        Person bikePerson = PopulationUtils.getFactory().createPerson(Id.createPersonId("bikePerson"));
+        Person walkPerson = PopulationUtils.getFactory().createPerson(Id.createPersonId("walkPerson"));
+
+
+        PopulationFactory factory = population.getFactory();
+        Leg carLeg = factory.createLeg(TransportMode.car);
+        Leg bikeLeg = factory.createLeg(TransportMode.bike);
+        Leg walkLeg = factory.createLeg(TransportMode.walk);
+        Link originLink = network.getLinks().get(Id.createLinkId("80"));
+        Link destinationLink = network.getLinks().get(Id.createLinkId("81"));
+
+        return population;
+    }
+
+
 }
+
