b45d5737798888df583cb2baa2db40ac26c81a0e
GregorRyb <43266258+GregorRyb@users.noreply.github.com>
2023-05-25 18:11:47 +0200

    Fix git mess with Bicycle Policies (#6)
    * add test comment
    
    * add another comment
    
    * fix most of the mess
    
    * re add NearestLinkChooser
    
    * remove warnings
    
    ---------
    
    Co-authored-by: GregorRyb <rybczak@vsp.tu-berlin.de>

---
+++ src/main/java/org/matsim/prepare/BicyclePolicies.java
@@ -0,0 +1,196 @@
+package org.matsim.prepare;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.glassfish.jaxb.core.v2.TODO;
+import org.locationtech.jts.geom.Geometry;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.application.MATSimAppCommand;
+import org.matsim.application.prepare.network.CleanNetwork;
+import org.matsim.contrib.bicycle.BicycleUtils;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.ShapeFileReader;
+import picocli.CommandLine;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * This class add bike policies to the network. And can be run with the following parameters for example
+ * --in
+ * "shared-svn\projects\GlaMoBi\matsim-input-files\gladbeck-v1.0.network_resolutionHigh-with-pt-with-bike.xml.gz"
+ * --on
+ * "shared-svn\projects\GlaMoBi\matsim-input-files\all-push-and-pull.network.xml.gz"
+ * --shp
+ * "shared-svn\projects\GlaMoBi\data\shp-files\Gladbeck.shp"
+ * --p
+ * SuperSmooth
+ * --p
+ * CyclewayEverywhere
+ * --p
+ * SuperFast
+ * --p
+ * CycleStreets
+ * comment to merge */
+@CommandLine.Command(name = "bicycle-policies")
+public class BicyclePolicies implements MATSimAppCommand {
+
+	private static final Logger log = LogManager.getLogger(BicyclePolicies.class);
+
+	private static final Set<String> majorRoadTypes = Set.of("motorway", "motorway_trunk", "trunk", "trunk_link", "primary", "primary_link", "secondary", "secondary_link", "tertiary", "tertiary_link");
+
+	@CommandLine.Option(names = {"--input-network", "--in" }, required = true)
+	private String inputNetwork;
+
+	@CommandLine.Option(names = {"--output-network", "--on" }, required = true)
+	private String outputNetwork;
+
+	@CommandLine.Option(names = { "--policy", "--p"}, required = true)
+	private Set<Policy> policies = new HashSet<>();
+
+	@CommandLine.Option(names = {"--bicycle-freespeed", "--bf"})
+	private double bicycleFreedspeed = 6.82; // taken from vehicles file in metropole-ruhr-scenario https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.mode-vehicles.xml
+
+	@SuppressWarnings("FieldMayBeFinal")
+	@CommandLine.Option(names = {"--shp"}, required = true)
+	private Path shp;
+
+	public static void main(String[] args) {
+		System.exit(new CommandLine(new BicyclePolicies()).execute(args));
+	}
+
+	@Override
+	public Integer call() {
+
+		log.info(policies.toString());
+
+		var geoFilter = ShapeFileReader.getAllFeatures(shp.toString()).stream()
+				.map(f -> (Geometry)f.getDefaultGeometry())
+				.findAny()
+				.orElseThrow();
+		var network = NetworkUtils.readNetwork(inputNetwork);
+
+		applyPolicyChanges(network, geoFilter, policies, bicycleFreedspeed);
+
+		NetworkUtils.writeNetwork(network, outputNetwork);
+		return 0;
+	}
+
+	public static void applyPolicyChanges(Network network, Geometry geoFilter, Set<Policy> policies, double bicycleFreedspeed) {
+		var filteredNetwork = network.getLinks().values().stream()
+				.filter(link -> !link.getAllowedModes().contains(TransportMode.pt)) // pt links are separate, so this test is sufficient
+				.filter(link -> geoFilter.covers(MGC.coord2Point(link.getFromNode().getCoord())) || geoFilter.covers(MGC.coord2Point(link.getToNode().getCoord())))
+				.collect(NetworkUtils.getCollector());
+
+		// in case one want to execute a combination of the policies the order in which those changes are applied matter
+		// at the moment
+		if (policies.contains(Policy.CycleStreets)) applyCycleStreets(filteredNetwork);
+		if (policies.contains(Policy.CyclewayEverywhere)) applyCyclewayEverywhere(filteredNetwork, bicycleFreedspeed);
+		if (policies.contains(Policy.SuperFast)) applySuperFast(filteredNetwork);
+		if (policies.contains(Policy.SuperSmooth)) applySuperSmooth(filteredNetwork);
+
+		for (var link : filteredNetwork.getLinks().values()) {
+
+			// replace links in original network with links from filtered network which we have altered.
+			network.removeLink(link.getId());
+			network.addLink(link);
+		}
+
+		MultimodalNetworkCleaner multimodalNetworkCleaner = new MultimodalNetworkCleaner(network);
+		multimodalNetworkCleaner.run(Collections.singleton(TransportMode.car));
+		multimodalNetworkCleaner.run(Collections.singleton(TransportMode.bike));
+	}
+
+	/**
+	 * Give all links asphalt surface, as well as a cycleway. This yields the highest scores and the highest speeds.
+	 */
+	private static void applySuperSmooth(Network network) {
+
+		log.info("Adding tags: cycleway:yes, surface:asphalt, smoothness:excellent to all links in the network");
+
+		for (var link : network.getLinks().values()) {
+			link.getAttributes().putAttribute("cycleway", "yes");
+			link.getAttributes().putAttribute("surface", "asphalt");
+			link.getAttributes().putAttribute("smoothness", "excellent");
+		}
+	}
+
+	/**
+	 * Change infrastructure speed factor on all streets to double the speed of bicycles
+	 */
+	private static void applySuperFast(Network network) {
+
+		log.info("Adding infrastructure speed factor of 1.0 to all links in the network.");
+
+		for (var link : network.getLinks().values()) {
+			link.getAttributes().putAttribute(BicycleUtils.BICYCLE_INFRASTRUCTURE_SPEED_FACTOR, 1.0);
+		}
+	}
+
+	/**
+	 * add a bicycle link next to each link in case it in not a bike only link anyway.
+	 */
+	private static void applyCyclewayEverywhere(Network network, double bicycleFreedspeed) {
+
+		log.info("Adding cycle ways to all links which were not bicycle ways already.");
+
+		var allowedModes = Set.of(TransportMode.bike);
+		var cycleways = network.getLinks().values().stream()
+				.filter(link -> !isBicycleOnly(link))
+				.map(link -> {
+					var id = link.getId().toString() + "_cyev";
+					var newLink = network.getFactory().createLink(Id.createLinkId(id), link.getFromNode(), link.getToNode());
+					newLink.getAttributes().putAttribute("type", "cycleway");
+					newLink.getAttributes().putAttribute("cycleway", "yes");
+					newLink.getAttributes().putAttribute("surface", "asphalt");
+					newLink.getAttributes().putAttribute("smoothness", "excellent");
+					newLink.getAttributes().putAttribute(BicycleUtils.BICYCLE_INFRASTRUCTURE_SPEED_FACTOR, 0.5);
+					//TODO
+					// this is needed because accessEgressModeToLinkPlusTimeConstant is still active.........
+					NetworkUtils.setLinkEgressTime(newLink, TransportMode.bike, 0.0);
+					NetworkUtils.setLinkAccessTime(newLink, TransportMode.bike, 0.0);
+					var origid = link.getAttributes().getAttribute("origid");
+					origid = origid == null ? "" : origid;
+					newLink.getAttributes().putAttribute("origid", origid);
+					newLink.setCapacity(10000);
+					newLink.setFreespeed(bicycleFreedspeed);
+					newLink.setAllowedModes(allowedModes);
+					return newLink;
+				})
+				.collect(Collectors.toSet());
+
+		for (var link : cycleways) {
+			network.addLink(link);
+		}
+	}
+
+	/**
+	 * Close minor roads for car traffic
+	 */
+	private static void applyCycleStreets(Network network) {
+
+		log.info("Converting all minor streets to cycle only streets");
+
+		var allowedModes = Set.of(TransportMode.bike);
+		network.getLinks().values().stream()
+				.filter(link -> link.getAttributes().getAttribute("type") != null)
+				.filter(link -> !majorRoadTypes.contains((String) link.getAttributes().getAttribute("type")))
+				.forEach(link -> link.setAllowedModes(allowedModes));
+	}
+
+	private static boolean isBicycleOnly(Link link) {
+		return link.getAllowedModes().size() == 1 && link.getAllowedModes().contains(TransportMode.bike);
+	}
+
+	public enum Policy {
+		SuperSmooth, CyclewayEverywhere, SuperFast, CycleStreets
+	}
+}
--- src/main/java/org/matsim/run/RunGladbeckScenario.java
+++ src/main/java/org/matsim/run/RunGladbeckScenario.java
@@ -7,6 +7,7 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.application.MATSimApplication;
 import org.matsim.application.analysis.emissions.AirPollutionByVehicleCategory;
 import org.matsim.application.analysis.emissions.AirPollutionSpatialAggregation;
@@ -20,8 +21,11 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.router.MultimodalLinkChooser;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.prepare.AssignPersonAttributes;
+import org.matsim.prepare.BicyclePolicies;
 import org.matsim.prepare.ScenarioCutOut;
 import org.matsim.run.policies.KlimaTaler;
 import org.matsim.run.policies.SchoolRoadsClosure;
@@ -30,11 +34,10 @@ import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 import picocli.CommandLine;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @CommandLine.Command(header = ":: Gladbeck Scenario ::", version = RunGladbeckScenario.VERSION)
-@MATSimApplication.Prepare({ScenarioCutOut.class, DownSamplePopulation.class, FixSubtourModes.class, XYToLinks.class, ExtractHomeCoordinates.class})
+@MATSimApplication.Prepare({ScenarioCutOut.class, DownSamplePopulation.class, FixSubtourModes.class, XYToLinks.class, ExtractHomeCoordinates.class, BicyclePolicies.class})
 @MATSimApplication.Analysis({NoiseAnalysis.class, AirPollutionByVehicleCategory.class, AirPollutionSpatialAggregation.class})
 public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 
@@ -60,10 +63,16 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 	@CommandLine.Option(names = "--klimaTaler", defaultValue = "0.0", description = "amount of money to give to a person to use pt, walk and bike")
 	double klimaTalerMoneyAmount;
 
-	//added this for the test
-	public RunGladbeckScenario(@Nullable Config config) {
-		super(config);
-	}
+    @CommandLine.Option(names = {"--policy", "--p"})
+    private Set<BicyclePolicies.Policy> policies = new HashSet<>();
+
+    @CommandLine.Option(names = {"--bicycle-freespeed", "--bf"})
+    private double bicycleFreedspeed = 6.82; // taken from vehicles file in metropole-ruhr-scenario https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.mode-vehicles.xml
+
+    //added this for the test
+    public RunGladbeckScenario(@Nullable Config config) {
+        super(config);
+    }
 
 	public RunGladbeckScenario() {
 		super(String.format("./scenarios/gladbeck-v1.0/input/gladbeck-%s-10pct.config.xml", VERSION));
@@ -73,10 +82,15 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 		MATSimApplication.run(RunGladbeckScenario.class, args);
 	}
 
-	@Override
-	protected Config prepareConfig(Config config) {
-		// Always switch off intermodal
-		this.intermodal = false;
+    @Override
+    protected Config prepareConfig(Config config) {
+
+        if (!policies.isEmpty() && !shp.isDefined()) {
+            throw new RuntimeException("A geo filter is required to apply policy changes to the network. Please add a path to a shape file by using the --shp option");
+        }
+
+        // Always switch off intermodal
+        this.intermodal = false;
 
 		// so we don´t use the rvr accessEgressModeToLinkPlusTimeConstant
 		config.plansCalcRoute().setAccessEgressType(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink);
@@ -126,12 +140,30 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 			new SchoolRoadsClosure().closeSchoolLinks(listOfSchoolLinks, scenario.getNetwork(), 800, 1700);
 		}
 
-		if (cyclingCourse == true) {
-			log.info("adding diffrent citizenships to the agents");
-			AssignPersonAttributes.assigningDifferentCitizenship(scenario, shp);
-		}
+        if (cyclingCourse) {
+            log.info("adding diffrent citizenships to the agents");
+            AssignPersonAttributes.assigningDifferentCitizenship(scenario, shp);
+        }
 
-	}
+        if (!policies.isEmpty()) {
+            BicyclePolicies.applyPolicyChanges(scenario.getNetwork(), shp.getGeometry(), policies, bicycleFreedspeed);
+            //delete routes from plans and linkId and facility id from activity
+			var fix = new FixSubtourModes();
+			for (var person : scenario.getPopulation().getPersons().values()) {
+				fix.run(person);
+				var plan = person.getSelectedPlan();
+				person.getPlans().clear();
+				PopulationUtils.resetRoutes(plan);
+				for (var element : plan.getPlanElements()) {
+					if (element instanceof Activity act) {
+						act.setFacilityId(null);
+						act.setLinkId(null);
+					}
+				}
+				person.addPlan(plan);
+			}
+        }
+    }
 
 	@Override
 	protected void prepareControler(Controler controler) {
@@ -142,6 +174,13 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 			KlimaTaler klimaTaler = new KlimaTaler(controler.getScenario().getConfig().plansCalcRoute().getBeelineDistanceFactors().get(TransportMode.walk), controler.getScenario().getNetwork(), klimaTalerMoneyAmount);
 			addKlimaTaler(controler, klimaTaler);
 		}
+
+        controler.addOverridingModule(new AbstractModule() {
+            @Override
+            public void install() {
+                bind(MultimodalLinkChooser.class).to(NearestLinkChooser.class);
+            }
+        });
 		super.prepareControler(controler);
 	}
 
