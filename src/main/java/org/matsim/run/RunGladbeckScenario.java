201a770004d7aeac3d3792c34d9ed8a714f9740a
GregorRyb <rybczak@vsp.tu-berlin.de>
2023-05-25 14:14:54 +0200

    finished with IntegrationTest

--- src/main/java/org/matsim/run/RunGladbeckScenario.java
+++ src/main/java/org/matsim/run/RunGladbeckScenario.java
@@ -5,23 +5,24 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.*;
 import org.matsim.application.MATSimApplication;
 import org.matsim.application.analysis.emissions.AirPollutionByVehicleCategory;
 import org.matsim.application.analysis.emissions.AirPollutionSpatialAggregation;
 import org.matsim.application.analysis.noise.NoiseAnalysis;
 import org.matsim.application.options.ShpOptions;
-import org.matsim.application.prepare.population.DownSamplePopulation;
-import org.matsim.application.prepare.population.ExtractHomeCoordinates;
-import org.matsim.application.prepare.population.FixSubtourModes;
-import org.matsim.application.prepare.population.XYToLinks;
+import org.matsim.application.prepare.population.*;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.router.MultimodalLinkChooser;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.prepare.AssignPersonAttributes;
 import org.matsim.prepare.BicyclePolicies;
+import org.matsim.prepare.DeleteRoutesFromPlans;
 import org.matsim.prepare.ScenarioCutOut;
 import org.matsim.run.policies.KlimaTaler;
 import org.matsim.run.policies.ReduceSpeed;
@@ -29,30 +30,26 @@ import org.matsim.run.policies.SchoolRoadsClosure;
 import org.matsim.utils.gis.shp2matsim.ShpGeometryUtils;
 import picocli.CommandLine;
 
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @CommandLine.Command(header = ":: Gladbeck Scenario ::", version = RunGladbeckScenario.VERSION)
 @MATSimApplication.Prepare({ScenarioCutOut.class, DownSamplePopulation.class, FixSubtourModes.class, XYToLinks.class, ExtractHomeCoordinates.class, BicyclePolicies.class})
 @MATSimApplication.Analysis({NoiseAnalysis.class, AirPollutionByVehicleCategory.class, AirPollutionSpatialAggregation.class})
 public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 
-	public static final String VERSION = "v1.0";
+    public static final String VERSION = "v1.0";
 
-	@CommandLine.Option(names = "--schoolClosure", defaultValue = "false", description = "measures to ban car on certain links")
-	boolean schoolClosure;
+    @CommandLine.Option(names = "--schoolClosure", defaultValue = "false", description = "measures to ban car on certain links")
+    boolean schoolClosure;
 
-	@CommandLine.Option(names = "--tempo30Zone", defaultValue = "false", description = "measures to reduce car speed to 30 km/h")
-	boolean tempo30Zone;
+    @CommandLine.Option(names = "--tempo30Zone", defaultValue = "false", description = "measures to reduce car speed to 30 km/h")
+    boolean tempo30Zone;
 
-	@CommandLine.Mixin
-	private ShpOptions shp;
+    @CommandLine.Mixin
+    private ShpOptions shp;
 
-	@CommandLine.Option(names = "--simplePtFlat", defaultValue = "false", description = "measures to allow everyone to have free pt")
-	private boolean simplePtFlat;
+    @CommandLine.Option(names = "--simplePtFlat", defaultValue = "false", description = "measures to allow everyone to have free pt")
+    private boolean simplePtFlat;
 
 	@CommandLine.Option(names = "--cyclingCourse", defaultValue = "false", description = "measures to increase the ")
 	boolean cyclingCourse;
@@ -60,11 +57,11 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 	@CommandLine.Option(names = "--klimaTaler", defaultValue = "0.0", description = "amount of money to give to a person to use pt, walk and bike")
 	double klimaTalerMoneyAmount;
 
-	@CommandLine.Option(names = { "--policy", "--p"}, required = true)
-	private Set<BicyclePolicies.Policy> policies = new HashSet<>();
+    @CommandLine.Option(names = {"--policy", "--p"}, required = false)
+    private Set<BicyclePolicies.Policy> policies = new HashSet<>();
 
-	@CommandLine.Option(names = {"--bicycle-freespeed", "--bf"})
-	private double bicycleFreedspeed = 6.82; // taken from vehicles file in metropole-ruhr-scenario https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.mode-vehicles.xml
+    @CommandLine.Option(names = {"--bicycle-freespeed", "--bf"})
+    private double bicycleFreedspeed = 6.82; // taken from vehicles file in metropole-ruhr-scenario https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/metropole-ruhr/metropole-ruhr-v1.0/input/metropole-ruhr-v1.0.mode-vehicles.xml
 
 	//added this for the test
 	public RunGladbeckScenario(@Nullable Config config) {
@@ -72,46 +69,43 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 	}
 
 	public RunGladbeckScenario() {
-		super(String.format("./scenarios/gladbeck-v1.0/input/gladbeck-%s-10pct.config.xml", VERSION));
-	}
+        super(String.format("./scenarios/gladbeck-v1.0/input/gladbeck-%s-10pct.config.xml", VERSION));
+    }
 
-	public static void main(String[] args) {
-		MATSimApplication.run(RunGladbeckScenario.class, args);
-	}
+    public static void main(String[] args) {
+        MATSimApplication.run(RunGladbeckScenario.class, args);
+    }
 
-	@Override
-	protected Config prepareConfig(Config config) {
+    @Override
+    protected Config prepareConfig(Config config) {
 
-		if (!policies.isEmpty() && !shp.isDefined()) {
-			throw new RuntimeException("A geo filter is required to apply policy changes to the network. Please add a path to a shape file by using the --shp option");
-		}
+        if (!policies.isEmpty() && !shp.isDefined()) {
+            throw new RuntimeException("A geo filter is required to apply policy changes to the network. Please add a path to a shape file by using the --shp option");
+        }
 
-		// Always switch off intermodal
-		this.intermodal = false;
+        // Always switch off intermodal
+        this.intermodal = false;
 
+        // so we don´t use the rvr accessEgressModeToLinkPlusTimeConstant --> still used currently maybe a good idea to model parking spaces
+        config.plansCalcRoute().setAccessEgressType(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink);
 
+        if (simplePtFlat) {
+            config.planCalcScore().getModes().get(TransportMode.pt).setDailyMonetaryConstant(0.0);
+        }
 
-		// so we don´t use the rvr accessEgressModeToLinkPlusTimeConstant
-		config.plansCalcRoute().setAccessEgressType(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink);
+        // this is needed for the school closure case
+        config.network().setTimeVariantNetwork(true);
 
-		if (simplePtFlat) {
-			config.planCalcScore().getModes().get(TransportMode.pt).setDailyMonetaryConstant(0.0);
-		}
+        return super.prepareConfig(config);
+    }
 
-		// this is needed for the school closure case
-		config.network().setTimeVariantNetwork(true);
+    @Override
+    protected void prepareScenario(Scenario scenario) {
+        super.prepareScenario(scenario);
 
-
-		return super.prepareConfig(config);
-	}
-
-	@Override
-	protected void prepareScenario(Scenario scenario) {
-		super.prepareScenario(scenario);
-
-		if (tempo30Zone) {
-			ReduceSpeed.implementPushMeasuresByModifyingNetworkInArea(scenario.getNetwork(), ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(shp.getShapeFile().toString())));
-		}
+        if (tempo30Zone) {
+            ReduceSpeed.implementPushMeasuresByModifyingNetworkInArea(scenario.getNetwork(), ShpGeometryUtils.loadPreparedGeometries(IOUtils.resolveFileOrResource(shp.getShapeFile().toString())));
+        }
 
 		if (schoolClosure) {
 			List<Id<Link>> listOfSchoolLinks = new ArrayList<>();
@@ -150,9 +144,40 @@ public class RunGladbeckScenario extends RunMetropoleRuhrScenario {
 		}
 
 	}
+        if (!policies.isEmpty()) {
+            BicyclePolicies.applyPolicyChanges(scenario.getNetwork(), shp.getGeometry(), policies, bicycleFreedspeed);
+            //delete routes from plans and linkId and facility id from activity
+            Iterator personIterator = scenario.getPopulation().getPersons().values().iterator();
+            while (personIterator.hasNext()) {
+                Person person = (Person) personIterator.next();
+                Plan selected = person.getSelectedPlan();
+                Iterator planIterator = Lists.newArrayList(person.getPlans()).iterator();
 
-	@Override
-	protected void prepareControler(Controler controler) {
+                while (planIterator.hasNext()) {
+                    Plan plan = (Plan) planIterator.next();
+                    if (plan != selected) {
+                        person.removePlan(plan);
+                    }
+
+                    Iterator planElementIterator = plan.getPlanElements().iterator();
+                    while (planElementIterator.hasNext()) {
+                        PlanElement el = (PlanElement) planElementIterator.next();
+                        if (el instanceof Leg) {
+                            ((Leg) el).setRoute(null);
+                        }
+                        if (el instanceof Activity) {
+                            ((Activity) el).setLinkId(null);
+                            ((Activity) el).setFacilityId(null);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Override
+    protected void prepareControler(Controler controler) {
 
 		//controler.getConfig().vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.abort);
 		if (klimaTalerMoneyAmount != 0.0) {
