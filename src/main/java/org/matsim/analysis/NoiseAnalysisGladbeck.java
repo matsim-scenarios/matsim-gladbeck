56fec188aac2e002011dc04e4096320426c12ee7
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-10-31 20:00:04 +0100

    add command line params for noise analysis

--- src/main/java/org/matsim/analysis/NoiseAnalysisGladbeck.java
+++ src/main/java/org/matsim/analysis/NoiseAnalysisGladbeck.java
@@ -6,6 +6,13 @@ import org.matsim.application.analysis.noise.NoiseAnalysis;
 public class NoiseAnalysisGladbeck {
 
     public static void main (String args []) {
+        if ( args==null || args.length==0 ) {
+            args = new String[]{
+                            "--directory", "../../runs-svn/glaMobi/freePt/",
+                            "--runId", "freePt_gladbeck-v1.0",
+                            "--input-crs", "EPSG:25832"
+            };
+        }
         new NoiseAnalysis().execute(args);
     }
 
