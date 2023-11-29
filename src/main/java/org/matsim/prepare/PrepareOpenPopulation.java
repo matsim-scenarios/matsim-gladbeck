aa20a181d3736766314b8645da4f51cf21ca1b0f
rakow <rakow@vsp.tu-berlin.de>
2023-11-29 20:26:11 +0100

    first version of script to adjust open population

--- src/main/java/org/matsim/prepare/PrepareOpenPopulation.java
+++ src/main/java/org/matsim/prepare/PrepareOpenPopulation.java
@@ -1,22 +1,22 @@
 package org.matsim.prepare;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.router.TripStructureUtils;
 import picocli.CommandLine;
 
 import java.nio.file.Path;
+import java.util.List;
 
 @CommandLine.Command(
         name = "open-population",
         description = "Prepare open population"
 )
 public class PrepareOpenPopulation implements MATSimAppCommand {
-
-    private static final Logger log = LogManager.getLogger(PrepareOpenPopulation.class);
 
     @CommandLine.Option(names = "--open-population", description = "Path to population using open data", required = true)
     private Path openPopulationPath;
@@ -33,9 +33,6 @@ public class PrepareOpenPopulation implements MATSimAppCommand {
 
     @Override
     public Integer call() throws Exception {
-
-        // TODO read open population
-        // replace coordinates from
 
         Population openPopulation = PopulationUtils.readPopulation(openPopulationPath.toString());
         Population calibratedPopulation = PopulationUtils.readPopulation(calibratedPopulationPath.toString());
@@ -62,8 +59,37 @@ public class PrepareOpenPopulation implements MATSimAppCommand {
      */
     private void prepare(Person targetPerson, Person sourcePerson) {
 
+        // Activities with type "other" were added synthetically, these coordinates are not copied
+        List<Activity> activities = TripStructureUtils.getActivities(sourcePerson.getSelectedPlan(), TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
 
-        // TODO
+        for (Plan plan : targetPerson.getPlans()) {
+
+            List<Activity> targetActs = TripStructureUtils.getActivities(plan, TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
+
+            int toActivityCounter = 0;
+            for (int fromActivityCounter = 0; fromActivityCounter < activities.size(); fromActivityCounter++) {
+                Activity fromActivity = activities.get(fromActivityCounter);
+                Activity toActivity = targetActs.get(toActivityCounter);
+
+                // Skip other and the second part of the actual activity (which are both not present in the target)
+                if (fromActivity.getType().startsWith("other")) {
+                    fromActivityCounter++;
+                    continue;
+                }
+
+                if (toActivity.getType().startsWith("other")) {
+                    Activity secondOccurrenceOfLastFromActivity = targetActs.get(toActivityCounter + 1);
+                    secondOccurrenceOfLastFromActivity.setCoord(activities.get(fromActivityCounter - 1).getCoord());
+
+                    toActivityCounter += 2;
+                    continue;
+                }
+
+                toActivity.setCoord(fromActivity.getCoord());
+                toActivityCounter++;
+            }
+
+            TripStructureUtils.getLegs(plan).forEach(leg -> leg.setRoute(null));
+        }
     }
-
 }
