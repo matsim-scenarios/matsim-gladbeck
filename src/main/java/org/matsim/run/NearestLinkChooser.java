a0ff592c7aa1700110228279cc7aebf6c9efa791
GregorRyb <rybczak@vsp.tu-berlin.de>
2025-02-05 12:06:20 +0100

    comment out NearestLinkChooser.java as it is not needed anymore

--- src/main/java/org/matsim/run/NearestLinkChooser.java
+++ src/main/java/org/matsim/run/NearestLinkChooser.java
@@ -1,4 +1,4 @@
-package org.matsim.run;
+/*package org.matsim.run;
 
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
@@ -16,5 +16,5 @@ public class NearestLinkChooser implements MultimodalLinkChooser {
 
         return NetworkUtils.getNearestLink(network, facility.getCoord());
     }
-}
+} */
 
